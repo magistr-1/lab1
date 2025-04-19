d1659cd8ef24a04a0e645010a37c9c9e632f3f67
magistr-1 <m4g1str123@gmail.com>
2025-04-19 18:15:41 +0300

    Add files via upload

---
+++ Lab_2/math_operations.py
@@ -0,0 +1,15 @@
+# math_operations.py
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        raise ValueError("Деление на ноль недопустимо!")
